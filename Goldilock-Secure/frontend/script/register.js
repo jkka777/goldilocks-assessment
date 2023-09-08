386dd516a24bc7203f916a61b35a6989712257c0
jkka777 <abhiram.kishore@gmail.com>
2023-09-08 14:16:53 +0530

    made modifications to register page

--- Goldilock-Secure/frontend/script/register.js
+++ Goldilock-Secure/frontend/script/register.js
@@ -1,6 +1,8 @@
 
 let catchElem = (e) => document.querySelector(e);
 
+let createElem = (e) => document.createElement(e);
+
 const url = `http://localhost:8880/user/save`;
 
 let register = catchElem('#reg-btn');
@@ -25,15 +27,58 @@ register.addEventListener('click', () => {
         body: JSON.stringify(data)
     })
         .then(res => {
+            console.log(res);
             if (res.status == 200) {
+                openModal();
                 return res.json();
             }
             console.log(res.status);
         })
         .then(data => {
             console.log(data);
-            if (data.status == 'success') {
-                window.location.href = 'login.html';
-            }
         })
+
+    catchElem('#uName').value = '';
+    catchElem('#uEmail').value = '';
+    catchElem('#uPass').value = '';
+    catchElem('#uRole').value = '';
+
 });
+
+let modal = catchElem(".modal");
+
+let openModal = () => {
+
+    modal.innerHTML = '';
+    modal.style.display = "block";
+
+    let modalContent = createElem("div");
+    modalContent.setAttribute("class", "modal-content");
+
+    let modalHeader = createElem("div");
+    modalHeader.setAttribute("class", "modal-header");
+
+    let modal_p = createElem("p");
+    modal_p.textContent = "Registration Successful! Please login.";
+
+    modalHeader.append(modal_p);
+
+    let modalFooter = createElem("div");
+    modalFooter.setAttribute("class", "modal-footer");
+
+    let modalYes = createElem("button");
+    modalYes.setAttribute("class", "modal-yes");
+    modalYes.textContent = "Login";
+    modalYes.addEventListener("click", () => {
+        window.location.href = 'login.html';
+        modal.style.display = "none";
+    });
+
+
+    modalFooter.append(modalYes);
+
+    modalContent.append(modalHeader, modalFooter);
+
+    modal.append(modalContent);
+
+};
